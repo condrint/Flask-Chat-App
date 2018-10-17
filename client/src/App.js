fdc41bf9e75b3604b8b4c689fcf60c70daf54230
condrint <condrint@gmail.com>
2018-10-16 21:32:57 -0700

    working on server file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
+import { io } from 'socketio';
 /*   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.3/socket.io.min.js"></script>
         <script type="text/javascript">
-          var socket = io.connect('http://' + document.domain + ':' + location.port);
-          socket.on( 'connect', function() {
-            socket.emit( 'my event', {
-              data: 'User Connected'
-            } )
+
             var form = $( 'form' ).on( 'submit', function( e ) {
               e.preventDefault()
               let user_name = $( 'input.username' ).val()
@@ -28,20 +24,42 @@ import './App.css';
           })
         </script>*/
 class App extends Component {
+  constructor(){
+    //connect the socket to the backend
+    var socket = io.connect(`http://${document.domain}:3000`);
+    socket.on( 'connect', function() {
+      //emit to socket listening for 'my event' 
+      console.log('i logged on')
+      socket.emit( 'my event', {
+        data: 'User Connected'
+      });
+    });
+
+    this.handleMessageSubmit = this.handleMessageSubmit.bind(this);
+
+  }
+
+  handleMessageSubmit(e){
+    e.preventDefault()
+    let user_name = 'asdf';
+    let user_input = 'asdf';
+    this.socket.emit( 'my event', {
+      user_name : user_name,
+      message : user_input
+    } )
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        
-        <h3 style='color: #ccc;font-size: 30px;'>No message yet..</h3>
-        <div class="message_holder"></div>
-
-        <form action="" method="POST">
+        <form action="" method="POST" onSubmit={this.handleMessageSubmit}>
           <input type="text" class="username" placeholder="User Name"/>
           <input type="text" class="message" placeholder="Messages"/>
           <input type="submit"/> 
         </form>
-        
-     
+        <div class="message_holder"></div>
       </div>
     );
   }
--- server.py
+++ server.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_from_directory, make_response, jsonify
 from flask_socketio import SocketIO
+import os, sys
 
+#initialize library variables
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
 
 @app.route('/')
-def sessions():
-    return render_template('session.html')
+def getMainPage():
+    print('/')
+    return send_from_directory('client/build', 'index.html')
 
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def router(path):
+    print('in 2nd router')
+    print(path, file=sys.stdout)
+    if 'api' in path:
+        pass
+    
+    
+    #retrieve static files
+    elif path and os.path.exists('client/build/' + path):
+        print(path, file=sys.stdout)
+        return send_from_directory('client/build', path)
 
+#socketIO code
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
@@ -21,5 +37,6 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     socketio.emit('my response', json, callback=messageReceived)
 
 
+#entry point
 if __name__ == '__main__':
     socketio.run(app, debug=True)
