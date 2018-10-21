b084f47fce44d2657294db81543d2b8562e1c97f
condrint <condrint@gmail.com>
2018-10-20 20:49:05 -0700

    added comments to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Messages } from './Messages';
 import io from 'socket.io-client/dist/socket.io';
+
+//document domain corresponds to the url in the browser
+//in our case it may be localhost when developing
+//and somewhere on heroku for deployment
 var socket = io(`http://${document.domain}:5000`);
 
 
@@ -8,39 +12,47 @@ var socket = io(`http://${document.domain}:5000`);
 class App extends Component {
   constructor(){
     super()
+    
+    //create state for component
     this.state ={
       messages: [['asdf', 'asdfd']],
       alias: '',
       message: '',
     }
 
+    //bind functions to this object
     this.handleMessageSubmit = this.handleMessageSubmit.bind(this);
     this.handleAliasChange = this.handleAliasChange.bind(this);
     this.handleMessageChange = this.handleMessageChange.bind(this);
     this.updateMessages = this.updateMessages.bind(this);
 
-    socket.on( 'connect', function() {
-      //emit to socket listening for 'my event' 
+    //instantiate socket events
+    socket.on('connect', function() {
+      //emit to socket listening for a connection
       socket.emit( 'user connected', {
         data: 'User Connected'
       });
     });
 
+    //received a new message
     socket.on( 'my response', (msg) => {
       this.updateMessages(msg);
     })
-
   }
 
 
 
   updateMessages(message){
+    //update the messages in state to include
+    //the message passed as a parameter
     let currentMessages = this.state.messages;
     currentMessages.push([message.user_name, message.message]);
     this.setState({messages:currentMessages});
   }
   
   handleMessageSubmit(e){
+    //extract data from html form and emit
+    //to socket code on backend
     e.preventDefault()
     let user_name = this.state.alias;
     let user_input = this.state.message;
@@ -51,12 +63,13 @@ class App extends Component {
     this.setState({ message: '' }); //clear message for user after sending
   }
 
+  //functions to keep react state consistent
+  //with current input
   handleAliasChange(e){ this.setState({alias:e.target.value}); } //as user types, update state to reflect change
   handleMessageChange(e){ this.setState({message:e.target.value}); } //as user types, update state to reflect change
 
 
   render() {
-    console.log(this.state.messages);
     let messages = this.state.messages;
     return (
       <div className="App">
