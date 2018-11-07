17c6f77c9147dd0ea411dbbd29844d1c0aba6873
condrint <condrint@gmail.com>
2018-11-06 22:32:28 -0800

    same

---
+++ client/build/precache-manifest.ae34c76ceba41d18b96b4910f3439bf9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f35fa6542103ae6019e1",
+    "url": "/static/js/main.f35fa654.chunk.js"
+  },
+  {
+    "revision": "715bfa15d0c677be9e55",
+    "url": "/static/js/1.715bfa15.chunk.js"
+  },
+  {
+    "revision": "0a4b36220c110d9586be8e803ec84576",
+    "url": "/index.html"
+  }
+];
