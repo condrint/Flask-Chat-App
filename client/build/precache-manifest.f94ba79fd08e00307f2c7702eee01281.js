833f3c4be507598eb478ad780b1bad823fd0e087
condrint <condrint@gmail.com>
2018-11-06 21:54:28 -0800

    adding build for deployment

---
+++ client/build/precache-manifest.f94ba79fd08e00307f2c7702eee01281.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b26affe6de6afcb28864",
+    "url": "/static/js/main.b26affe6.chunk.js"
+  },
+  {
+    "revision": "715bfa15d0c677be9e55",
+    "url": "/static/js/1.715bfa15.chunk.js"
+  },
+  {
+    "revision": "d1b6a2b4b9593bf881b2bf1fe7409149",
+    "url": "/index.html"
+  }
+];
